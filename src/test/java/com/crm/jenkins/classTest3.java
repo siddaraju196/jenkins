5e428d32ee2465234cb0d3d2a0f32a407b502728
siddaraju196 <91187173+siddaraju196@users.noreply.github.com>
2022-04-14 16:02:13 +0530

    Update classTest3.java

--- src/test/java/com/crm/jenkins/classTest3.java
+++ src/test/java/com/crm/jenkins/classTest3.java
@@ -2,7 +2,7 @@ package com.crm.jenkins;
 
 import org.testng.annotations.Test;
 
-public class classTest3 {
+public class class3Test {
 	@Test
 	public void test() {
 		System.out.println("system test2");

6c57761f32f69d33e4b52b675f3258484032950f
kiran ke <kiran ke@DESKTOP-12VNEQH>
2022-04-14 13:39:24 +0530

    first commit

--- src/test/java/com/crm/jenkins/classTest2.java
+++ src/test/java/com/crm/jenkins/classTest2.java
@@ -1,9 +1,11 @@
 package com.crm.jenkins;
 
+import org.testng.annotations.Test;
+
 public class classTest2 {
-	
+	@Test
 	public void test() {
-		System.out.println("system test1");
+		System.out.println("system test3");
 	}
 
 }
--- src/test/java/com/crm/jenkins/classTest3.java
+++ src/test/java/com/crm/jenkins/classTest3.java
@@ -1,9 +1,11 @@
 package com.crm.jenkins;
 
+import org.testng.annotations.Test;
+
 public class classTest3 {
-	
+	@Test
 	public void test() {
-		System.out.println("system test1");
+		System.out.println("system test2");
 	}
 
 }
